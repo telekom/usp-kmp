a29b6fca9fb1f04f05092e4837018d9ae888b354
Ulrich Kemp <ulrich.kemp@telekom.de>
2024-04-19 15:10:25 +0200

    Added dmtest script with AXACT MQTT configuration, not working

--- scripts/axact_create_controller.sh
+++ scripts/axact_create_controller.sh
@@ -1,7 +1,28 @@
 #!/bin/sh
 # Script to create a local AXACT controller with the dmtest tool
+
+# ------- WEB SOCKET CONFIGURATION -----------------------------------------------------------------
 ./dmtest oa Device.LocalAgent.Controller.
 INSTANCE=1
 ./dmtest sv Device.LocalAgent.Controller.${INSTANCE}.Alias usp-demo
 ./dmtest sv Device.LocalAgent.Controller.${INSTANCE}.EndpointID proto::usp-demo
 ./dmtest sv Device.LocalAgent.Controller.${INSTANCE}.Enable 1
+
+
+# ------- MQTT CONFIGURATION -----------------------------------------------------------------------
+./dmtest oa Device.LocalAgent.Controller.
+INSTANCE=3
+./dmtest oa Device.LocalAgent.Controller.${INSTANCE}.MTP.
+./dmtest sv Device.LocalAgent.Controller.${INSTANCE}.MTP.1.Protocol MQTT
+./dmtest sv Device.LocalAgent.Controller.${INSTANCE}.MTP.1.MQTT.Topic axact-agent-topic
+./dmtest sv Device.LocalAgent.Controller.${INSTANCE}.MTP.1.Enable 1
+./dmtest sv Device.LocalAgent.Controller.${INSTANCE}.Enable 1
+
+./dmtest oa Device.MQTT.Client.
+INSTANCE=1
+./dmtest sv Device.MQTT.Client.${INSTANCE}.BrokerAddress home.kempmobil.de
+./dmtest sv Device.MQTT.Client.${INSTANCE}.BrokerPort 8883
+./dmtest sv Device.MQTT.Client.${INSTANCE}.Username usp-demo
+./dmtest sv Device.MQTT.Client.${INSTANCE}.Password secret
+./dmtest sv Device.MQTT.Client.${INSTANCE}.Enable 1
+
